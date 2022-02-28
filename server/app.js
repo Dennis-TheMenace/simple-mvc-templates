7a7bafc4b7ff321a39b2d47f8bd3dcccf1fad3b7
Alex Gray (RIT Student) <arg2399@rit.edu>
2022-02-28 14:36:27 -0500

    MVC Template Demo

--- server/app.js
+++ server/app.js
@@ -3,8 +3,11 @@ const express = require('express');
 const compression = require('compression');
 const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
+const expressHandlebars = require('express-handlebars');
 
 const router = require('./router.js');
+const req = require('express/lib/request');
+const { dirname } = require('path');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -16,6 +19,14 @@ app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.engine('handlebars', expressHandlebars.engine(
+{
+  defaultLayout: '',
+}));
+
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/../views`);
 
 router(app);
 
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,29 +1,58 @@
 // There is no reason for the name here except as an
+
+const { filter } = require("compression");
+
 // example of how to set something for the POST
 let name = 'unknown';
 
 const hostIndex = (req, res) => {
-
+  return res.render('index',
+  {
+    title: 'Home',
+    pageName: 'Home Page',
+    userName: name,
+  });
 };
 
 const hostPage1 = (req, res) => {
+  const myArray = ['Austin', 'Cody', 'Binky', 'Aron'];
+  const filtered = myArray.filter(x => x[0] === 'A');//x => x.length <= 4
 
+  return res.render('page1',
+  {
+    title: 'page 1',
+    names: filtered,
+  });
 };
 
 const hostPage2 = (req, res) => {
-
+  return res.render('page2');
 };
 
 const getName = (req, res) => {
-
+  res.json({name});
 };
 
 const setName = (req, res) => {
+  if(!req.body.firstname || !req.body.lastname)
+  {
+    return res.status(400).json(
+    {
+      error: 'firstname and lastname are both required',
+      id: 'setNameMissingParam',
+    });
+  }
+  
+  name = `${req.body.firstname} ${req.body.lastname}`;
 
+  return res.json({name});
 };
 
 const notFound = (req, res) => {
-
+  return res.render('notFound',
+  {
+    page: req.url,
+  });
 };
 
 module.exports = {
